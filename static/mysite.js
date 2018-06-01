1b7bde4889f21d116aaf3dd965b0ed543fc07369
kpucci <kpucci@ansys.com>
2018-06-01 12:27:21 -0400

    started responsive design

--- static/mysite.js
+++ static/mysite.js
@@ -10,7 +10,7 @@ function setup()
     var sticky = navbar.offsetTop;
 
     // document.getElementById("all-button").focus();
-    loadAll();
+    // loadAll();
 
     // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
     function myFunction(){
@@ -86,127 +86,127 @@ function makeHandler(httpRequest, retCode, callback)
 /**
 * Get project list
 */
-function loadAll()
-{
-    // console.log("Get Categories");
-	makeReq("GET", "/cats/all", 200, showAllProjects);
-}
-
-/**
-* Get project list
-*/
-function loadWeb()
-{
-    // console.log("Get Categories");
-	makeReq("GET", "/cats/web", 200, showWebProjects);
-}
-
-/**
-* Get project list
-*/
-function loadEmbedded()
-{
-    // console.log("Get Categories");
-	makeReq("GET", "/cats/embedded", 200, showEmbeddedProjects);
-}
-
-/**
-* Get project list
-*/
-function loadArt()
-{
-    // console.log("Get Categories");
-	makeReq("GET", "/cats/art", 200, showArtProjects);
-}
-
-function showWebProjects(responseText)
-{
-	document.getElementById("web-button").className = "btn-clicked";
-	document.getElementById("all-button").className = "";
-	document.getElementById("art-button").className = "";
-	document.getElementById("embedded-button").className = "";
-	showProjects(responseText, "web");
-}
-
-function showEmbeddedProjects(responseText)
-{
-	document.getElementById("embedded-button").className = "btn-clicked";
-	document.getElementById("all-button").className = "";
-	document.getElementById("art-button").className = "";
-	document.getElementById("web-button").className = "";
-	showProjects(responseText, "embedded");
-}
-
-function showAllProjects(responseText)
-{
-	document.getElementById("all-button").className = "btn-clicked";
-	document.getElementById("web-button").className = "";
-	document.getElementById("art-button").className = "";
-	document.getElementById("embedded-button").className = "";
-	showProjects(responseText, "all");
-}
-
-function showArtProjects(responseText)
-{
-	document.getElementById("art-button").className = "btn-clicked";
-	document.getElementById("all-button").className = "";
-	document.getElementById("web-button").className = "";
-	document.getElementById("embedded-button").className = "";
-	showProjects(responseText, "art");
-}
-
-function showProjects(responseText, cat)
-{
-    console.log("repopulating!");
-	var projects = JSON.parse(responseText);
-    var viewer = document.getElementById("port-view");
-
-    while(viewer.hasChildNodes()){
-        viewer.removeChild(viewer.childNodes[0]);
-    }
-
-    if(projects.length > 0)
-    {
-		viewer.className = "portfolio-viewer";
-    	var p, img_path, proj_img, proj_container, proj_name, proj_link;
-
-    	for(p in projects){
-            proj_name = projects[p]["name"];
-    		img_path = projects[p]["img"];
-
-            proj_link = document.createElement("A");
-            proj_link.href = "/" + proj_name.toLowerCase();
-
-            proj_container = document.createElement("div");
-            proj_container.className = "project-image-container animate";
-            proj_container.id = proj_name;
-
-            proj_img = document.createElement("IMG");
-            proj_img.src = "/static/" + img_path;
-            proj_img.alt = proj_name;
-            proj_img.className = "project-image";
-
-            proj_container.appendChild(proj_img);
-            proj_link.appendChild(proj_container);
-            viewer.appendChild(proj_link);
-    	}
-    }
-	else
-	{
-		viewer.className = "empty-portfolio";
-		var no_proj = document.createElement("p");
-		no_proj.className = "section-text";
-
-		if(cat == "all")
-			no_proj.innerHTML = "Sorry, no projects have been uploaded yet!";
-		else
-			no_proj.innerHTML = "Sorry, no " + cat + " projects have been uploaded yet!";
-
-		viewer.appendChild(no_proj);
-	}
-
-    console.log("finished!");
-}
+// function loadAll()
+// {
+//     // console.log("Get Categories");
+// 	makeReq("GET", "/cats/all", 200, showAllProjects);
+// }
+//
+// /**
+// * Get project list
+// */
+// function loadWeb()
+// {
+//     // console.log("Get Categories");
+// 	makeReq("GET", "/cats/web", 200, showWebProjects);
+// }
+//
+// /**
+// * Get project list
+// */
+// function loadEmbedded()
+// {
+//     // console.log("Get Categories");
+// 	makeReq("GET", "/cats/embedded", 200, showEmbeddedProjects);
+// }
+//
+// /**
+// * Get project list
+// */
+// function loadArt()
+// {
+//     // console.log("Get Categories");
+// 	makeReq("GET", "/cats/art", 200, showArtProjects);
+// }
+//
+// function showWebProjects(responseText)
+// {
+// 	document.getElementById("web-button").className = "btn btn-clicked";
+// 	document.getElementById("all-button").className = "btn";
+// 	document.getElementById("art-button").className = "btn";
+// 	document.getElementById("embedded-button").className = "btn";
+// 	showProjects(responseText, "web");
+// }
+//
+// function showEmbeddedProjects(responseText)
+// {
+// 	document.getElementById("embedded-button").className = "btn btn-clicked";
+// 	document.getElementById("all-button").className = "btn";
+// 	document.getElementById("art-button").className = "btn";
+// 	document.getElementById("web-button").className = "btn";
+// 	showProjects(responseText, "embedded");
+// }
+//
+// function showAllProjects(responseText)
+// {
+// 	document.getElementById("all-button").className = "btn btn-clicked";
+// 	document.getElementById("web-button").className = "btn";
+// 	document.getElementById("art-button").className = "btn";
+// 	document.getElementById("embedded-button").className = "btn";
+// 	showProjects(responseText, "all");
+// }
+//
+// function showArtProjects(responseText)
+// {
+// 	document.getElementById("art-button").className = "btn btn-clicked";
+// 	document.getElementById("all-button").className = "btn";
+// 	document.getElementById("web-button").className = "btn";
+// 	document.getElementById("embedded-button").className = "btn";
+// 	showProjects(responseText, "art");
+// }
+//
+// function showProjects(responseText, cat)
+// {
+//     console.log("repopulating!");
+// 	var projects = JSON.parse(responseText);
+//     var viewer = document.getElementById("port-view");
+//
+//     while(viewer.hasChildNodes()){
+//         viewer.removeChild(viewer.childNodes[0]);
+//     }
+//
+//     if(projects.length > 0)
+//     {
+// 		viewer.className = "portfolio-viewer";
+//     	var p, img_path, proj_img, proj_container, proj_name, proj_link;
+//
+//     	for(p in projects){
+//             proj_name = projects[p]["name"];
+//     		img_path = projects[p]["img"];
+//
+//             proj_link = document.createElement("A");
+//             proj_link.href = "/" + proj_name.toLowerCase();
+//
+//             proj_container = document.createElement("div");
+//             proj_container.className = "project-image-container animate";
+//             proj_container.id = proj_name;
+//
+//             proj_img = document.createElement("IMG");
+//             proj_img.src = "/static/" + img_path;
+//             proj_img.alt = proj_name;
+//             proj_img.className = "project-image";
+//
+//             proj_container.appendChild(proj_img);
+//             proj_link.appendChild(proj_container);
+//             viewer.appendChild(proj_link);
+//     	}
+//     }
+// 	else
+// 	{
+// 		viewer.className = "empty-portfolio";
+// 		var no_proj = document.createElement("p");
+// 		no_proj.className = "section-text";
+//
+// 		if(cat == "all")
+// 			no_proj.innerHTML = "Sorry, no projects have been uploaded yet!";
+// 		else
+// 			no_proj.innerHTML = "Sorry, no " + cat + " projects have been uploaded yet!";
+//
+// 		viewer.appendChild(no_proj);
+// 	}
+//
+//     console.log("finished!");
+// }
 
 // setup load event
 window.addEventListener("load", setup, true);
